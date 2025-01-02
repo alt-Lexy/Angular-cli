043274171f4ffb7aa388b4cffb9139ba33cc892a
alexia <alexia.lievin@gmail.com>
2025-01-02 16:02:23 +0100

    feat(app-cli): prepare reuse card component

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class CardComponent {
 
+  company: string = "Entreprise du soleil";
+  activity: string = "entreprise spécialisé dans le tourisme";
+  note: string = "52.01";
+  risk: string = "C";
 }
