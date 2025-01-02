c8b5afa52e3ad40c83e9976a82231c8804270e12
alexia <alexia.lievin@gmail.com>
2025-01-02 15:31:32 +0100

    feat(api): component card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CardComponent } from './components/card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
